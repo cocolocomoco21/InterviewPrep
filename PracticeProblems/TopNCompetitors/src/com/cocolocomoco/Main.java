7f9ad331ccea9649bd70e519fef1256e5d3d100b
Matt Henricks <mchenricks21@gmail.com>
2022-01-31 22:51:46 -0800

    Update TopN filtering to work - use custom Competitor class

--- PracticeProblems/TopNCompetitors/src/com/cocolocomoco/Main.java
+++ PracticeProblems/TopNCompetitors/src/com/cocolocomoco/Main.java
@@ -1,3 +1,13 @@
+/**
+ * Note: this approach is not the most optimal way to solve this problem. I opted for a Java8 stream approach, but for
+ * time complexity this is not the best, as there are redundant iterations and string lookups. The more optimal way is to:
+ * - create a HashSet of competitors
+ * - iterate over each word of each review, lookup if each word is in the HashSet of competitors
+ * - it word is a competitor, count occurrence in count HashMap and mark competitor as seen (separate HashSet)
+ * - after operating over all reviews, return sorted sublist. this can be done via priority queue or heap to maintain
+ *   count, and then removing the top n, or from filtering/sorting a list and limiting the return.
+ */
+
 package com.cocolocomoco;
 
 import java.util.*;
@@ -20,10 +30,8 @@ public class Main {
         );
 
         List<String> topCompetitors = topNumCompetitors(numCompetitors, topNCompetitors, competitors, numReviews, reviews);
-        System.out.println("done");
-
+        System.out.println(topCompetitors.toString());
     }
-
 
     public static List<String> topNumCompetitors(int numCompetitors,
                                           int topNCompetitors,
@@ -39,16 +47,25 @@ public class Main {
             throw new IllegalArgumentException("Amount of requested top competitors is greater than amount of total competitors");
         }
 
+        // Previous code, kept for reference
         // Build map of competitor to occurrence in reviews
-        HashMap<String, List<Integer>> mapCompetitorToReviewOccurrence = buildCompetitorOccurrenceMap(competitors, reviews);
+        //HashMap<String, List<Integer>> mapCompetitorToReviewOccurrence = buildCompetitorOccurrenceMap(competitors, reviews);
+        //Set<Map.Entry<String, List<Integer>>> result = mapCompetitorToReviewOccurrence.entrySet().stream().sorted(Comparator.comparingInt(value -> value.getValue().size())).limit(topNCompetitors).collect(Collectors.toSet());
+
+        List<Competitor> competitorsOccurrenceList = buildCompetitorOccurrenceList(competitors, reviews);
+        competitorsOccurrenceList
+                .sort((o1, o2) -> o1.reviewsAppearedIn.size() == o2.reviewsAppearedIn.size()
+                        ? o1.name.compareTo(o2.name)
+                        : o2.reviewsAppearedIn.size() - o1.reviewsAppearedIn.size());
 
 
-
-
-        List<String> result = mapCompetitorToReviewOccurrence.entrySet().stream().sorted(Comparator.comparingInt(value -> value.getValue().size())).limit(topNCompetitors).map(x -> x.getKey()).collect(Collectors.toList());
-        return result;
+        return competitorsOccurrenceList.stream()
+                .map(c -> c.name)
+                .limit(topNCompetitors)
+                .collect(Collectors.toList());
     }
 
+    // Previous code, kept for reference
     private static HashMap<String, List<Integer>> buildCompetitorOccurrenceMap(List<String> competitors, List<String> reviews) {
         HashMap<String, List<Integer>> mapCompetitorToReviewOccurrence = new HashMap<>();
 
@@ -70,5 +87,42 @@ public class Main {
         }
 
         return mapCompetitorToReviewOccurrence;
+    }
+
+    private static List<Competitor> buildCompetitorOccurrenceList(List<String> competitors, List<String> reviews) {
+        List<Competitor> competitorOccurrenceList = new ArrayList<>();
+
+        // Populate each competitor occurrence
+        for (String competitor : competitors) {
+            // Create new Competitor
+            Competitor currentCompetitor = new Competitor(competitor);
+            for (int i = 0; i < reviews.size(); i++) {
+                String review = reviews.get(i).toLowerCase();
+
+                if (review.contains(competitor)) {
+                    // Add index of review string to signify occurrence in that string
+                    currentCompetitor.addReviewOccurrence(i);
+                }
+            }
+
+            competitorOccurrenceList.add(currentCompetitor);
+        }
+
+        return competitorOccurrenceList;
+    }
+}
+
+class Competitor {
+    // TODO in an actual interview, don't access these via the raw field. Make them private and use getters
+    String name;
+    Set<Integer> reviewsAppearedIn;
+
+    Competitor(String name) {
+        this.name = name;
+        this.reviewsAppearedIn = new HashSet<>();
+    }
+
+    public boolean addReviewOccurrence(int i) {
+        return this.reviewsAppearedIn.add(i);
     }
 }
