b4488f0ef15d0e6412b3449d610825a69b8705d2
Matt Henricks <mchenricks21@gmail.com>
2022-06-20 22:05:56 -0700

    New Pascals solution

--- PracticeProblems/PascalsTriangle/src/com/cocolocomoco/Main.java
+++ PracticeProblems/PascalsTriangle/src/com/cocolocomoco/Main.java
@@ -7,10 +7,14 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
-	    Solution solution = new Solution();
-
-        int numRows = 1;
-        List<List<Integer>> result = solution.generate(numRows);
+//        Solution solution = new Solution();
+//        int numRows = 1;
+//        List<List<Integer>> result = solution.generate(numRows);
+//        System.out.println(result.toString());
+        
+        Solution2 solution2 = new Solution2();
+        int numRows = 6;
+        List<List<Integer>> result = solution2.generate(numRows);
         System.out.println(result.toString());
     }
 }
@@ -56,5 +60,46 @@ class Solution {
         }
 
         return currentRow;
+    }
+}
+
+class Solution2 {
+    public List<List<Integer>> generate(int numRows) {
+        if (numRows < 1) {
+            return null;
+        }
+
+        List<List<Integer>> pascals = new ArrayList<>();
+        pascals.add(0, List.of(1));
+
+        // Early exit if available
+        if (numRows == 1) {
+            return pascals;
+        }
+
+        int currentRow = 2;
+        while (currentRow <= numRows) {
+            List<Integer> currentRowValues = new ArrayList<>();
+            List<Integer> previousRowValues = pascals.get(currentRow - 2);
+
+            // Add leading 1 for all rows
+            currentRowValues.add(1);
+
+            // Add values in "above" row at i-1 and i to get current value
+            for (int i = 1; i < currentRow - 1; i++) {
+                currentRowValues.add(previousRowValues.get(i - 1) + previousRowValues.get(i));
+            }
+
+            // Add trailing 1 for all rows
+            currentRowValues.add(1);
+
+            // Commit row to end result
+            pascals.add(currentRowValues);
+
+            currentRow++;
+        }
+
+        // Return end result
+        return pascals;
     }
 }
