673481ccebf954a28aad4d24b3c14d4d0d522525
Matt Henricks <mchenricks21@gmail.com>
2022-06-22 20:56:37 -0700

    Rerun CoinChange

--- PracticeProblems/CoinChange/src/com/cocolocomoco/Main.java
+++ PracticeProblems/CoinChange/src/com/cocolocomoco/Main.java
@@ -6,17 +6,21 @@ import java.util.stream.Collectors;
 public class Main {
 
     public static void main(String[] args) {
+        int[] coins = new int[] {2};
+        int amount = 3;
+
 //        int[] coins = new int[] {186,419,83,408};
 //        int amount = 6249;
 
 //        int[] coins = new int[] {1, 2, 5};
 //        int amount = 11;
 
-        int[] coins = new int[] {1, 4, 6};
-        int amount = 8;
+//        int[] coins = new int[] {1, 4, 6};
+//        int amount = 8;
 
-        Solution solution = new Solution();
+//        Solution solution = new Solution();
 //        SolutionII solution = new SolutionII();
+        SolutionIII solution = new SolutionIII();
         int fewestCoins = solution.coinChange(coins, amount);
 
         System.out.println(fewestCoins);
@@ -104,5 +108,60 @@ class Solution {
 
         this.mapRemainingToLeastCoins.put(remainingAmount, min == Integer.MAX_VALUE ? -1 : min);
         return this.mapRemainingToLeastCoins.getOrDefault(remainingAmount, -1);
+    }
+}
+
+
+class SolutionIII {
+    HashMap<Integer, Integer> mapRemainingAmountToLeastCoins;
+
+    public SolutionIII (){
+        this.mapRemainingAmountToLeastCoins = new HashMap<>();
+    }
+
+    public int coinChange(int[] coins, int amount) {
+        if (amount == 0) {
+            return 0;
+        }
+
+        for (int coin : coins) {
+            this.mapRemainingAmountToLeastCoins.put(coin, 1);
+        }
+
+        if (this.mapRemainingAmountToLeastCoins.containsKey(amount)) {
+            return 1;
+        }
+
+        return lookup(coins, amount);
+    }
+
+    private int lookup(int[] coins, int remainingAmount) {
+        // 0 or negative remainingAmount is invalid - this remainingAmount does not have a valid coin scenario
+        if (remainingAmount < 1) {
+            return -1;
+        }
+
+        // If already discovered value for this remainingAmount, return it instead of researching
+        if (this.mapRemainingAmountToLeastCoins.containsKey(remainingAmount)) {
+            return this.mapRemainingAmountToLeastCoins.get(remainingAmount);
+        }
+
+        int minCoinsForRemainingAmount = Integer.MAX_VALUE;
+        for (int coin : coins) {
+            int minCoins = lookup(coins, remainingAmount - coin);
+            if (minCoins != -1 && minCoins < minCoinsForRemainingAmount) {
+                // Add one to account for current coin
+                minCoinsForRemainingAmount = minCoins + 1;
+            }
+        }
+
+        // We found a valid minCoins amount, update it
+        if (minCoinsForRemainingAmount != Integer.MAX_VALUE) {
+            this.mapRemainingAmountToLeastCoins.put(remainingAmount, minCoinsForRemainingAmount);
+        } else {
+            this.mapRemainingAmountToLeastCoins.put(remainingAmount, -1);
+        }
+
+        return this.mapRemainingAmountToLeastCoins.get(remainingAmount);
     }
 }
